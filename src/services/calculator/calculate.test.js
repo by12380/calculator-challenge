a6af8427f5d2190900996a5e2d52e3feec772221
Brian Young <by12380@gmail.com>
2019-10-25 00:06:02 -0700

    Req #7 - Update calculator test to meet req #7

--- src/services/calculator/calculate.test.js
+++ src/services/calculator/calculate.test.js
@@ -87,4 +87,11 @@ describe('calculate', () => {
     expect(calculate('//*\n1* 5000\n10000, 2')).toBe(3)
     expect(calculate('//.\n1.200\n300, 2')).toBe(503)
   })
+
+  test('Should return correct sum when seperated by multi-char custom delimiter', () => {
+    expect(calculate('//[#]\n123, 321 \n 111# 0')).toBe(555)
+    expect(calculate('//[!!]\n0, 0!! 0\n 0, 0')).toBe(0)
+    expect(calculate('//[***]\n1*** 5000\n10000, 2')).toBe(3)
+    expect(calculate('//[.!*]\n1.!*200\n300, 2')).toBe(503)
+  })
 })
